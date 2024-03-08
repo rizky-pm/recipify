31406fd2304e3a374d689bf7d89fa29a2e33df16
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2024-03-08 17:48:06 +0700

    Added @tanstack/react-query library

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,16 @@
-// import { PropsWithChildren } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { router } from '@/routes';
 
-// ({ children }: PropsWithChildren)
+const queryClient = new QueryClient({});
+
 const Providers = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
