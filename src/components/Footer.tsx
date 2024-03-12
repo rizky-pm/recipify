1cb1d1662daafbdf7c328f0b199ca317dde2b692
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2024-03-13 01:19:29 +0700

    Improved footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ import MaxWidthWrapper from './MaxWidthWrapper';
 const Footer = () => {
   return (
     <footer className='py-6 bg-primary text-background text-center'>
-      <MaxWidthWrapper>
-        <div className='flex flex-wrap items-center justify-between'>
+      <MaxWidthWrapper className='flex flex-col space-y-4'>
+        <div className='flex flex-wrap items-center justify-between flex-col space-y-4'>
           <div className='w-full'>
-            <h2 className='text-lg font-semibold mb-2'>About</h2>
+            <h2 className='text-lg font-semibold mb-1'>About</h2>
             <p className='text-sm'>
               Recipify is a culinary perosnal project powered by The Meal DB
               API, offering a diverse selection of recipes from around the
@@ -16,7 +16,7 @@ const Footer = () => {
             </p>
           </div>
           <div className='w-full md:w-1/2'>
-            <h2 className='text-lg font-semibold mb-2'>Contact Me</h2>
+            <h2 className='text-lg font-semibold mb-1'>Contact Me</h2>
 
             <div className='flex space-x-4 justify-center'>
               <a
@@ -29,7 +29,7 @@ const Footer = () => {
               </a>
 
               <a
-                href={'https://linkedin.com/in/rizky-p-mahendra/'}
+                href={'https://github.com/rizky-pm'}
                 target='_blank'
                 rel='noopener noreferrer'
                 className='group flex gap-1 items-center text-foreground hover:text-primary hover:bg-background w-12 h-12 justify-center rounded-full transition-all'
@@ -38,7 +38,7 @@ const Footer = () => {
               </a>
 
               <a
-                href={'https://linkedin.com/in/rizky-p-mahendra/'}
+                href={'mailto:rizkymahendra2346@gmail.com'}
                 target='_blank'
                 rel='noopener noreferrer'
                 className='group flex gap-1 items-center text-foreground hover:text-primary hover:bg-background w-12 h-12 justify-center rounded-full transition-all'
@@ -47,8 +47,7 @@ const Footer = () => {
               </a>
             </div>
           </div>
-        </div>
-        <div className='mt-4'>
+
           <p className='text-sm'>© 2024 Recipify. All rights reserved.</p>
         </div>
       </MaxWidthWrapper>
